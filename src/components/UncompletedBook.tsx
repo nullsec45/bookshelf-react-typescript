a769fbedda9c85fafd48a27333951953cbb98c7c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-30 10:24:25 +0700

    component UncompletedBook

---
+++ src/components/UncompletedBook.tsx
@@ -0,0 +1,15 @@
+const UncompletedBook = () => {
+    return (
+        <>
+            <section className="book_shelf" id="incomplete_container">
+                <h2>Belum selesai dibaca</h2>
+
+                <div id="incomplete_bookshelf_list" className="book_list">
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default UncompletedBook

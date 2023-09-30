96b3382089770d20fb9485b086993b37cef912f0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-30 10:23:42 +0700

    component CompletedBook

---
+++ src/components/CompletedBook.tsx
@@ -0,0 +1,13 @@
+const CompletedBook = () => {
+    return (
+        <>
+            <section className="book_shelf" id="complete_container">
+                <h2>Selesai dibaca</h2>
+                <div id="complete_bookshelf_list" className="book_list">
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CompletedBook

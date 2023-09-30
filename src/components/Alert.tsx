2e0bfd387fc84c7d04dca230f92c7df97e9b5cc7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-30 10:23:28 +0700

    component Alert

---
+++ src/components/Alert.tsx
@@ -0,0 +1,11 @@
+const Alert = () => {
+    return (
+        <>
+            <section id="empty_book">
+                <h2 id="title"><i>Belum ada buku yang ditambahkan :)</i></h2>
+            </section>
+        </>
+    )
+}
+
+export default Alert

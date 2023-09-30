69a6c79e21c89a701b3526bc9880c1fe6a697304
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-30 10:23:51 +0700

    component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <>
+            <header className="head_bar">
+                <h1 className="head_bar__title">Bookshelf Apps</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header

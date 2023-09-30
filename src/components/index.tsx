c28eedf0d6eda3199bf71f50b8bfd18b9a3fd72c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-30 10:24:38 +0700

    indexex of component

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import Alert from "./Alert";
+import Header from "./Header";
+import Input from "./Input";
+import SearchBook from "./SearchBook";
+import CompletedBook from "./CompletedBook";
+import UncompletedBook from "./UncompletedBook";
+
+export {
+    Alert,
+    Header,
+    Input,
+    SearchBook,
+    CompletedBook,
+    UncompletedBook
+}
